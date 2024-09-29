314d24a727faa9f109028fa601d91f472fbecbf8
jonusgreen <86344954+jonusgreen@users.noreply.github.com>
2024-09-29 19:10:30 +0300

    frontend set up

---
+++ client/src/components/propertyDetail/PropertyDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './propertydetail.module.css'
+export const Propertydetail = () => {
+  return (
+    <div>PropertyDetails</div>
+  )
+}
+export default Propertydetail
---
+++ client/src/components/signin/Signin.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './signin.module.css'
+export const Signin = () => {
+  return (
+    <div>Signin</div>
+  )
+}
+export default Signin
